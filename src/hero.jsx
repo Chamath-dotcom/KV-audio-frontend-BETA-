35e8a4e65b6bfdecb4cfa2da56b753a0072f5a71
Chamath Kalhara <ckalhara360@gmail.com>
2025-01-02 00:12:31 +0530

    add main BG✅

--- src/hero.jsx
+++ src/hero.jsx
@@ -3,7 +3,8 @@ import './hero.css';
 function Hero() {
   return (
      <div className='hero-div'>
-     
+        <img src="./hero_img.svg" alt="" />
+        <div className='nav-sec'></div>
      </div>
   );
 }
