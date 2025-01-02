cd53dc63be68d9880f855dc3eedd9657f101a7e5
Chamath Kalhara <ckalhara360@gmail.com>
2025-01-03 01:26:42 +0530

    create main btn✅

--- src/hero.jsx
+++ src/hero.jsx
@@ -4,7 +4,18 @@ function Hero() {
   return (
      <div className='hero-div'>
         <img src="./hero_img.svg" alt="" />
-        <div className='nav-sec'></div>
+        <div className='nav-sec'>
+         <img src="./logo.svg" alt="" className='logo'/>
+         <div className='acctions'>
+            <button className='cart-btn'>
+               <img src="cart.svg" alt="" />
+            </button>
+            <button className='login-btn'>LOGIN</button>
+            <button className='profile'>
+               <img src="./user-profile-icon.svg" alt="" />
+            </button>
+          </div>
+        </div>
      </div>
   );
 }
